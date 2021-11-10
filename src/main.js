f0cc6a06233caf693024bb1274c752405f29589f
hatso <mmorpg3636@naver.com>
2021-11-10 21:08:44 +0900

    add gitignore file and express Router work 11/10

--- src/main.js
+++ src/main.js
@@ -1,140 +1,58 @@
 // @ts-check
-/* eslint-disable no -restricted-syntax */
 
-/**
- * @typedef Person
- *
- * @property {number} age
- * @property {string} city
- * @property {string | string[]} [pet]
- */
+/** eslint-disable no-console */
 
-/** @type {Person[]} */
-const people = [
-  {
-    age: 20,
-    city: "서울",
-    pet: ["cat", "dog"],
-  },
-  {
-    age: 40,
-    city: "부산",
-  },
-  {
-    age: 31,
-    city: "대구",
-    pet: ["cat", "dog"],
-  },
-  {
-    age: 36,
-    city: "서울",
-  },
-  {
-    age: 27,
-    city: "부산",
-    pet: "cat",
-  },
-  {
-    age: 24,
-    city: "서울",
-    pet: "dog",
-  },
-]
+const express = require("express")
+const { request } = require("http")
+const userRouter = express.Router()
+//const bodyparser = require("body-parser")
+const exp = require("constants")
 
-/**
- * 다음 문제들을 풀어봅시다.
- *
- * A. 30대 미만이 한 명이라도 사는 모든 도시
- * B. 각 도시별로 개와 고양이를 키우는 사람의 수
- */
+const app = express()
+app.use(express.json())
 
-function solveA() {
-  /** @type {string[]} */
-  const cities = []
+const PORT = 5000
 
-  for (const person of people) {
-    if (person.age < 30) {
-      if (!cities.find((city) => person.city === city)) {
-        cities.push(person.city)
-      }
-    }
-  }
+userRouter.get("/", (req, res) => {
+  res.send("User list")
+})
 
-  return cities
-}
-function solveAModern() {
-  const allCities = people.filter(({ age }) => age < 30).map(({ city }) => city)
-  const set = new Set(allCities) //배열을 Set()을 이용해 집합으로 변경
-  return Array.from(set) //집합을 배열로 바꾸어 return
+const USERS = {
+  15: {
+    nickname: "foo",
+  },
 }
 
-// console.log("solveA ", solveA())
-// console.log("solveAModern", solveAModern())
+userRouter.param("id", (req, res, next, value) => {
+  console.log("id parameter", value)
+  // @ts-ignore
+  req.user = USERS[value]
+  next()
+})
 
-/** B. 각 도시별로 개와 고양이를 키우는 사람의 수 */
-/** @typedef {Object.<string, Object.<string, number>>} PetsOfCities */
-function solveB() {
-  /** @type {PetsOfCities} */
-  const result = {}
+userRouter.get("/:id", (req, res) => {
+  console.log("userRouter get ID")
+  // @ts-ignore
+  res.send(req.user)
+})
 
-  for (const person of people) {
-    const { city, pet: petOrPets } = person
+userRouter.post("/", (req, res) => {
+  // Register user
+  res.send("User registered.")
+})
 
-    if (petOrPets) {
-      const petsOfCity = result[city] || {}
-      if (typeof petOrPets === "string") {
-        const pet = petOrPets
+userRouter.post("/:id/nickname", (req, res) => {
+  // req.body: {"nickname": "bar"}
+  // @ts-ignore
+  const { user } = req
+  const { nickname } = req.body
 
-        const origNumPetOfCity = petsOfCity[pet] || 0
-        petsOfCity[pet] = origNumPetOfCity + 1
-      } else {
-        for (const pet of petOrPets) {
-          const origNumPetOfCity = petsOfCity[pet] || 0
-          petsOfCity[pet] = origNumPetOfCity + 1
-        }
-      }
-      result[city] = petsOfCity
-    }
-  }
-  return result
-}
+  user.nickname = nickname
+  res.send(`User nickname updated: ${nickname}`)
+})
 
-function solveBModern() {
-  return people
-    .map(({ pet: petOrPets, city }) => {
-      const pets =
-        (typeof petOrPets === "string" ? [petOrPets] : petOrPets) || []
+app.use("/users", userRouter)
 
-      return {
-        city,
-        pets,
-      }
-      /**
-       * [
-       *  [
-       *    ["서울", "cat"],
-       *    ["서울", "dog"],
-       *  ],
-       *  [
-       *    ["부산", "dog"],
-       *  ],
-       * ]
-       */
-    })
-    .flatMap(({ city, pets }) => pets.map((pet) => [city, pet]))
-    .reduce((/** @type {PetsOfCities} */ result, [city, pet]) => {
-      if (!city || !pet) {
-        return result
-      }
-
-      return {
-        ...result,
-        [city]: {
-          ...result[city],
-          [pet]: (result[city]?.[pet] || 0) + 1,
-        },
-      }
-    }, {})
-}
-console.log("solveB", solveB())
-console.log("solveBModern", solveBModern())
+app.listen(PORT, () => {
+  console.log(`The Express server is listening at port: ${PORT}`)
+})
